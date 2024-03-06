cd28679dd563b9eb3e866da916f3ddf5dcb2f34e
Mamani <mamani.developer@gmail.com>
2024-03-06 17:30:55 -0400

    Implement the function make negative

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
+import 'package:return_negative/make_negative.dart';
+
 void main() {
-  print('Hello, World!');
+  print('Enter a number');
+  num n = num.parse(stdin.readLineSync().toString());
+  print(makeNegative(n));
 }
