c7c41d14743366db1e77ae50f832f31cb92b528e
Mamani <mamani.developer@gmail.com>
2024-03-08 19:15:22 -0400

    Implement test for function make negative

---
+++ test/make_negative_test.dart
@@ -0,0 +1,19 @@
+import 'package:return_negative/make_negative.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('$makeNegative()', () {
+    test('to 1', () {
+      expect(makeNegative(1), equals(-1));
+    });
+    test('to -5', () {
+      expect(makeNegative(-5), equals(-5));
+    });
+    test('to 0', () {
+      expect(makeNegative(0), equals(0));
+    });
+    test('to 0.12', () {
+      expect(makeNegative(0.12), equals(-0.12));
+    });
+  });
+}
